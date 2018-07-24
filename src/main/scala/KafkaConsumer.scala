8b76b7385cffb29e2a64855ab3bbfd75752128ee
hongdd <jn_hdd@163.com>
2018-07-24 09:53:56 +0800

    Initial commit

---
+++ src/main/scala/KafkaConsumer.scala
@@ -0,0 +1,41 @@
+import java.util.{Collections, Properties}
+
+import org.apache.kafka.clients.consumer.{ConsumerConfig, KafkaConsumer}
+import org.apache.kafka.common.serialization.StringDeserializer
+import scala.collection.JavaConverters._  /* 隐式转换 将java collection转为scala */
+
+object KafkaConsumer {
+  def ZK_CONN = "192.168.56.171:2181"
+  def BOOTSTRAP = "192.168.56.171:9092"
+  def GROUP_ID = "test_scala_consumer_group"
+  def TOPIC = "test"
+
+  def main(args: Array[String]): Unit = {
+    println("开始读取kafka数据")
+
+    val consumer = new KafkaConsumer[String, String](kafkaConfig)
+    consumer.subscribe(Collections.singleton(TOPIC))
+
+    consumer.seekToBeginning(consumer.assignment())
+    val records = consumer.poll(10000)
+    //    println(records.count())
+    for (record <- records.asScala) {
+      println(record)
+    }
+
+    consumer.close()
+  }
+
+  def kafkaConfig(): Properties = {
+    val props = new Properties()
+    props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG /**/, BOOTSTRAP)
+    props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG /**/, classOf[StringDeserializer].getName)
+    props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, classOf[StringDeserializer].getName)
+    props.put(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID)
+    //    props.put(ConsumerConfig.GROUP_ID_CONFIG, UUID.randomUUID().toString) /* 随机groupId从头获取数据 */
+    props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true.toString)
+    props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,
+      "earliest" /* latest表示接受接收最大的offset(即最新消息),earliest表示最小offset,即从topic的开始位置消费所有消息. */)
+    props
+  }
+}
---
+++ src/main/scala/KafkaProducer.scala
@@ -0,0 +1,46 @@
+import java.util.Properties
+
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerConfig, ProducerRecord}
+import org.apache.kafka.common.serialization.StringSerializer
+
+object KafkaProducer {
+  def main(args: Array[String]): Unit = {
+    val brokers = "192.168.56.171:9092"
+    val topic = "test"
+
+    val props = new Properties()
+    props.put("metadata.broker.list", brokers)
+    props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG /* 同"key.serializer"必须配置*/,
+      classOf[StringSerializer].getName)
+    props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG /* 同"value.serializer"必须配置*/,
+      classOf[StringSerializer].getName)
+    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG /* 同"bootstrap.servers" 必须配置*/, brokers)
+    props.put("request.required.acks", "1")
+    props.put("producer.type", "async")
+
+    val producer = new KafkaProducer[String, String](props)
+
+    for (i <- 0 to 10) {
+      producer.send(new ProducerRecord(topic, "key-" + i, "64.242.88.10 - - [07/Mar/2004:16:05:49 -0800] \"GET /twiki/bin/edit/Main/Double_bounce_sender?topicparent=Main.ConfigurationVariables HTTP/1.1\" 401 12846"))
+      System.out.println(System.currentTimeMillis())
+      Thread.sleep(3000)
+    }
+
+    producer.close()
+
+    /*
+     * kafka 旧的写法,多种函数已被注释不可用
+     *
+     * val props = new Properties()
+     * props.put("metadata.broker.list", brokers)
+     * props.put("serializer.class", classOf[StringEncoder].getName)
+     * props.put("request.required.acks", "1")
+     * props.put("producer.type", "async")
+//    val config = new ProducerConfig(props)
+//    val producer = new Producer[String, String](config)
+//
+//    val message = new KeyedMessage[String, String](topic, "1", "some thing that is wrong")
+//    producer.send(message)
+    */
+  }
+}
