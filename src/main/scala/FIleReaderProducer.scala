9f27e0bd3c23c4555fb7ee1d205e67f3bbb2ad3a
hongdd <jn_hdd@163.com>
2018-07-26 15:38:48 +0800

    file producer

--- src/main/scala/FIleReaderProducer.scala
+++ src/main/scala/FIleReaderProducer.scala
@@ -3,7 +3,9 @@ import java.util.Properties
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerConfig, ProducerRecord}
 import org.apache.kafka.common.serialization.StringSerializer
 
-object KafkaProducer {
+import scala.io.Source
+
+object FIleReaderProducer {
   def main(args: Array[String]): Unit = {
     val brokers = "192.168.56.171:9092"
     val topic = "test"
@@ -20,28 +22,12 @@ object KafkaProducer {
 
     val producer = new KafkaProducer[String, String](props)
 
-    for (i <- 0 to 1000) {
-//      producer.send(new ProducerRecord(topic, "key-" + i, "64.242.88.10 - - [07/Mar/2004:16:05:49 -0800] \"GET /twiki/bin/edit/Main/Double_bounce_sender?topicparent=Main.ConfigurationVariables HTTP/1.1\" 401 12846"))
-      producer.send(new ProducerRecord(topic, "key-" +i, "hello world"))
-      System.out.println(System.currentTimeMillis())
+    Source.fromFile("src/main/resources/README.md").getLines().foreach { line =>
+      println(System.currentTimeMillis())
+      producer.send(new ProducerRecord(topic, "key-" + System.currentTimeMillis(), line.toString))
       Thread.sleep(200)
     }
 
     producer.close()
-
-    /*
-     * kafka 旧的写法,多种函数已被注释不可用
-     *
-     * val props = new Properties()
-     * props.put("metadata.broker.list", brokers)
-     * props.put("serializer.class", classOf[StringEncoder].getName)
-     * props.put("request.required.acks", "1")
-     * props.put("producer.type", "async")
-//    val config = new ProducerConfig(props)
-//    val producer = new Producer[String, String](config)
-//
-//    val message = new KeyedMessage[String, String](topic, "1", "some thing that is wrong")
-//    producer.send(message)
-    */
   }
 }
